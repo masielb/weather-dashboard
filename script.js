0f827493b2c0cf99e811620d171c3e53959b06b6
Masiel Bautista <masiel5@users.noreply.github.com>
2020-08-01 17:18:43 -0400

    cleared input box

--- script.js
+++ script.js
@@ -1,95 +1,97 @@
+var APIKey = "95d1f8c9874dee694f44538d063ccfc8";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=" + APIKey;
+
 $(document).ready(function() {
-    $("#search-button").on("click", function() {
-      var searchValue = $("#search-value").val();
-  
-      // clear input box
-  
-      searchWeather(searchValue);
-    });
-  
-    $(".history").on("click", "li", function() {
-      searchWeather($(this).text());
-    });
-  
-    function makeRow(text) {
-      var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
-      $(".history").append(li);
-    }
-  
-    function searchWeather(searchValue) {
-      $.ajax({
-        type: "",
-        url: "" + searchValue + "",
-        dataType: "json",
-        success: function(data) {
-          // create history link for this search
-          if (history.indexOf(searchValue) === -1) {
-            history.push(searchValue);
-            window.localStorage.setItem("history", JSON.stringify(history));
-      
-            makeRow(searchValue);
-          }
-          
-          // clear any old content
-  
-          // create html content for current weather
-  
-          // merge and add to page
-          
-          // call follow-up api endpoints
-          getForecast(searchValue);
-          getUVIndex(data.coord.lat, data.coord.lon);
-        }
-      });
-    }
+  $("#search-button").on("click", function() {
+    var searchValue = $("#search-value").val();
     
-    function getForecast(searchValue) {
-      $.ajax({
-        type: "",
-        url: "" + searchValue + "",
-        dataType: "json",
-        success: function(data) {
-          // overwrite any existing content with title and empty row
+    $("#search-value").val(""); // clears input box
+
+    searchWeather(searchValue);
+  });
+
+  $(".history").on("click", "li", function() {
+    searchWeather($(this).text());
+  });
+
+  function makeRow(text) {
+    var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+    $(".history").append(li);
+  }
+
+  function searchWeather(searchValue) {
+    $.ajax({
+      type: queryURL,
+      url: "" + searchValue + "",
+      dataType: "json",
+      success: function(data) {
+        // create history link for this search
+        if (history.indexOf(searchValue) === -1) {
+          history.push(searchValue);
+          window.localStorage.setItem("history", JSON.stringify(history));
+    
+          makeRow(searchValue);
+        }
+        
+        // clear any old content
+
+        // create html content for current weather
+
+        // merge and add to page
+        
+        // call follow-up api endpoints
+        getForecast(searchValue);
+        getUVIndex(data.coord.lat, data.coord.lon);
+      }
+    });
+  }
   
-          // loop over all forecasts (by 3-hour increments)
-          for (var i = 0; i < data.list.length; i++) {
-            // only look at forecasts around 3:00pm
-            if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-              // create html elements for a bootstrap card
-              
-  
-              // merge together and put on page
-            }
+  function getForecast(searchValue) {
+    $.ajax({
+      type: "",
+      url: "" + searchValue + "",
+      dataType: "json",
+      success: function(data) {
+        // overwrite any existing content with title and empty row
+
+        // loop over all forecasts (by 3-hour increments)
+        for (var i = 0; i < data.list.length; i++) {
+          // only look at forecasts around 3:00pm
+          if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+            // create html elements for a bootstrap card
+            
+
+            // merge together and put on page
           }
         }
-      });
-    }
-  
-    function getUVIndex(lat, lon) {
-      $.ajax({
-        type: "",
-        url: "" + lat + "&lon=" + lon,
-        dataType: "json",
-        success: function(data) {
-          var uv = $("<p>").text("UV Index: ");
-          var btn = $("<span>").addClass("btn btn-sm").text(data.value);
-          
-          // change color depending on uv value
-          
-          $("#today .card-body").append(uv.append(btn));
-        }
-      });
-    }
-  
-    // get current history, if any
-    var history = JSON.parse(window.localStorage.getItem("history")) || [];
-  
-    if (history.length > 0) {
-      searchWeather(history[history.length-1]);
-    }
-  
-    for (var i = 0; i < history.length; i++) {
-      makeRow(history[i]);
-    }
-  });
-  
+      }
+    });
+  }
+
+  function getUVIndex(lat, lon) {
+    $.ajax({
+      type: "",
+      url: "" + lat + "&lon=" + lon,
+      dataType: "json",
+      success: function(data) {
+        var uv = $("<p>").text("UV Index: ");
+        var btn = $("<span>").addClass("btn btn-sm").text(data.value);
+        
+        // change color depending on uv value
+        
+        $("#today .card-body").append(uv.append(btn));
+      }
+    });
+  }
+
+  // get current history, if any
+  var history = JSON.parse(window.localStorage.getItem("history")) || [];
+
+  if (history.length > 0) {
+    searchWeather(history[history.length-1]);
+  }
+
+  for (var i = 0; i < history.length; i++) {
+    makeRow(history[i]);
+  }
+});
